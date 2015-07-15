8d1629f90a896c79282bf27e2881c4a148192dae
jhernandezn <sidlors@github.com>
2015-07-15 11:32:37 -0500

    "En desarrollo"

---
+++ 13-Transactions/practica-13/jee-ejb/src/main/java/mx/certificatic/paractica/servicio/AltaInstructor.java
@@ -0,0 +1,30 @@
+package mx.certificatic.paractica.servicio;
+
+import javax.ejb.EJB;
+import javax.ejb.Local;
+import javax.ejb.LocalBean;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+import mx.certificatic.paractica.servicio.api.AltaInstructorLocal;
+import mx.certificatic.paractica.servicio.api.AltaInstructorRemote;
+import mx.certificatic.practica.persistencia.daos.InstructorDAO;
+
+/**
+ * Session Bean implementation class AltaInstructor
+ */
+@Stateless
+@LocalBean
+@Local(AltaInstructorLocal.class)
+@Remote(AltaInstructorRemote.class)
+public class AltaInstructor{
+
+	
+	@EJB
+	InstructorDAO dao;
+	
+	
+	
+	
+
+}
