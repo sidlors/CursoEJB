13bca4c3a78c81b7778d22c6714402676dce8dc1
jhernandezn <sidlors@github.com>
2015-07-16 19:41:04 -0500

    Front de la practica 7

---
+++ 11-Timer/practica7-ear/jee-war/src/main/java/org/primefaces/showcase/view/button/ButtonView.java
@@ -0,0 +1,19 @@
+package org.primefaces.showcase.view.button;
+ 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+ 
+@ManagedBean
+public class ButtonView {
+     
+    public void buttonAction(ActionEvent actionEvent) {
+        addMessage("Welcome to Primefaces!!");
+    }
+     
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
